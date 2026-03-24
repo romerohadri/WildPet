9211f82896ffafb2a38f2575397c894f0590e788
hderomero <hromeroa23@fpcoruna.afundacion.org>
2026-03-24 18:30:00 +0100

    P2.1: crear tabla usuarios

---
+++ init_database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS wildpet;
+USE wildpet;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
