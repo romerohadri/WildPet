549efcf16c69da24e6905ece676ad66647c2b959
hderomero <hromeroa23@fpcoruna.afundacion.org>
2026-04-24 11:30:00 +0200

    P7.4: botón pasar al carrito en mis favoritos

--- MisFavoritos.php
+++ MisFavoritos.php
@@ -1,2 +1,6 @@
-<?php include("conexion.php"); session_start(); 
-echo "<h1>Mis Favoritos</h1>"; ?>
+<?php 
+include("conexion.php"); 
+session_start(); 
+echo "<h1>Mis Favoritos</h1>"; 
+?>
+<button onclick="window.location.href='add_favorites_to_cart.php'">Pasar al carrito</button>
