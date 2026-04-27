4cd8d689ed3287423425eaf8b103d39d435f2aab
hderomero <hromeroa23@fpcoruna.afundacion.org>
2026-04-27 09:45:00 +0200

    P7.6: mostrar líneas de cada pedido

--- Pedidos.php
+++ Pedidos.php
@@ -5,3 +5,6 @@ echo "<h1>Mis Pedidos</h1>";
 $usuario_id = $_SESSION["usuario_id"];
 $res = $conn->query("SELECT * FROM pedidos WHERE usuario_id = $usuario_id"); 
 ?>
+<?php while($row = $res->fetch_assoc()) { 
+    echo "<div>Pedido #".$row["id"]." - Total: ".$row["total"]."€</div>"; 
+} ?>
