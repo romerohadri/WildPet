d3bf86aba7a886d71c71cd5d3dd8ebd25057ccb5
hderomero <hromeroa23@fpcoruna.afundacion.org>
2026-04-29 14:55:00 +0200

    P7.8: mostrar observaciones en pedidos

--- Pedidos.php
+++ Pedidos.php
@@ -8,3 +8,7 @@ $res = $conn->query("SELECT * FROM pedidos WHERE usuario_id = $usuario_id");
 <?php while($row = $res->fetch_assoc()) { 
     echo "<div>Pedido #".$row["id"]." - Total: ".$row["total"]."€</div>"; 
 } ?>
+<?php 
+// P7.8: Mostrar observaciones
+echo "<p>Observaciones: ".$row["observaciones"]."</p>"; 
+?>
