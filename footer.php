df56ba8bc66442dbeb630f384d297a76677be143
hderomero <hromeroa23@fpcoruna.afundacion.org>
2026-05-10 16:09:34 +0200

    P9.1: crear footer reutilizable

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+  <div class="footer-inner">
+    <div class="footer-layout">
+      <div class="footer-block footer-block-1">
+        <a href="Homepage.php">Inicio</a>
+        <span>·</span>
+        <a href="Homepage.php#sobre-nosotros">Sobre Nosotros</a>
+        <span>·</span>
+        <a href="Homepage.php#contacto">Contacto</a>
+        <span>·</span>
+        <a href="MiCuenta.php">Mi Cuenta</a>
+      </div>
+      <div class="footer-block footer-block-2">
+        <a href="Perros.php">Perros</a>
+        <span>·</span>
+        <a href="Gatos.php">Gatos</a>
+        <span>·</span>
+        <a href="Pajaros.php">Pájaros</a>
+        <span>·</span>
+        <a href="Peces.php">Peces</a>
+      </div>
+      <div class="footer-block footer-block-3">
+        <span class="footer-copy">© 2026 WildPet</span>
+      </div>
+      <div class="footer-block footer-block-4 footer-right">
+        <i class="fa-brands fa-facebook-f" aria-label="Facebook" title="Facebook"></i>
+        <i class="fa-brands fa-instagram" aria-label="Instagram" title="Instagram"></i>
+        <i class="fa-brands fa-x-twitter" aria-label="X" title="X"></i>
+      </div>
+    </div>
+  </div>
+</footer>
